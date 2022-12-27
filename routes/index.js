c0796f0388dde66ed6c7374fff801f7542c6ba53
Teje8 <jteruel8@gmail.com>
2022-12-27 19:31:20 -0300

    agrego rutas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const productosRoutes = require('./producto')
+const artistasRoutes = require('./artista')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json();
+  res.render('index', {title: 'LUROCOTATTO'});
 });
+
+router.use('/productos', productosRoutes)
+router.use('/artistas', artistasRoutes)
+
 
 module.exports = router;
