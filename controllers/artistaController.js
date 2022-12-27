ae0a2bf8a1c01a9c19a53d1520db4a472a5ebc69
Teje8 <jteruel8@gmail.com>
2022-12-27 19:33:36 -0300

    creacion de controladores de artistas y productos

---
+++ controllers/artistaController.js
@@ -0,0 +1,150 @@
+const Artista = require('../models/Artista')
+
+const artistaController = {
+
+    todosArtistas: async(req, res) => {
+        let artistas
+        try{
+            artistas = await Artista.find()
+            if(artistas){
+                res.status(200).json({
+                    message: 'Estos son todos los artistas',
+                    response: artistas,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: "No se encontraron artistas",
+                    success: false
+                })
+            }
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'Se produjo un error',
+                success: false
+            })
+        }
+    },
+
+    unArtista: async(req, res) => {
+        const {id} = req.params
+        try{
+            let artista = await Artista.findOne({_id:id})
+            if(artista){
+                res.status(200).json({
+                    message: 'Artista encontrado',
+                    response: artista,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'Artista no encontrado',
+                    success: false
+                })
+            }
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'Se produjo un error',
+                success: false
+            })
+        }
+    },
+
+    agregarArtista: async(req, res) => {
+        const { nombre, imagen, descripcion } = req.body
+
+        try{
+            // if(req.usuario.role === "admin"){
+            // } else {
+            //     res.status(401).json({
+            //         message: 'No autorizado',
+            //         success: true
+            //     })
+            // }
+            let artista = await new Artista(req.body).save()
+            res.status(201).json({
+                message: 'Nuevo artista agregado con exito',
+                response: artista._id,
+                success: true
+            })
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'No se pudo crear el artista',
+                success: false
+            })
+        }
+    },
+
+    editarArtista: async(req, res) => {
+        const {id} = req.params
+        const {role} = req.usuario
+        let putArtista = {}
+        let currentArtista
+        try{
+            if(putArtista){
+                currentArtista = await Artista.findOne({_id:id})
+                let { nombre,
+                        imagen,
+                        descripcion} = currentArtista
+
+                        if(role === "admin"){
+                            putArtista = await Artista.findOneAndUpdate({_id:id}, currentArtista, {new: true})
+                            res.status(200).json({
+                                message: "Artista editado con exito",
+                                response: putArtista,
+                                success: true
+                            })
+                        } else {
+                            res.status(401).json({
+                                message: "No autorizado",
+                                success: true
+                            })
+                        }
+            } else {
+                res.status(404).json({
+                    message: "No se encontro el artista",
+                    success: false
+                })
+            }
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "Se produjo un error",
+                success: false
+            })
+        }
+    },
+
+    eliminarArtista: async(req, res) => {
+        const {id} = req.params
+        // const {role} = req.usuario
+        
+        try{
+            // if(role === "admin"){
+            // } else {
+            //     res.status(401).json({
+            //         message: "No autorizado",
+            //         success: true
+            //     })
+            // }
+            await Artista.findOneAndDelete({_id:id})
+            res.status(200).json({
+                message: "Artista eliminado con exito",
+                success: true
+            })
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: "Se produjo un error",
+                success: false
+            })
+        }
+    }
+
+
+}
+
+module.exports = artistaController
---
+++ controllers/productoController.js
@@ -0,0 +1,104 @@
+const Producto = require('../models/Producto')
+
+const productoController = {
+
+    crearProducto: async(req, res) => {
+        const {nombre, imagen, descripcion, precio} = req.body
+
+        try{
+            let producto = await new Producto(nombre, imagen, descripcion, precio ,...req.body).save()
+            res.status(201).json({
+                message: 'Producto creado',
+                response: producto._id,
+                success: true
+            })
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'No se pudo crear el producto',
+                success: false
+            })
+        }
+    },
+
+    unProducto: async(req, res) =>{
+        const {id} = req.params
+        try{
+            let producto = await Producto.findOne({_id:id})
+            if(producto){
+                res.status(200).json({
+                    message: 'Producto encontrado',
+                    response: producto,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'No se encontro el producto',
+                    success: false
+                })
+            }
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'Se produjo un error',
+                success: false
+            })
+        }
+    },
+
+    productos: async(req, res) => {
+        const query = {}
+        let productos
+        if(req.query.producto){
+            let regExp = new RegExp(`^${req.query.producto}`, "i")
+            query.producto = regExp
+        }
+        try{
+            productos = await Producto.find(query)
+            if(productos){
+                res.status(200).json({
+                    message: 'Estos son todos los productos',
+                    response: productos,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: "No hay productos",
+                    success: false
+                })
+            }
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'Se produjo un error',
+                success: false
+            })
+        }
+    },
+
+    eliminarProducto: async(req, res) => {
+        const {id} = req.params
+        try{
+            let producto = await Producto.findOneAndDelete({_id:id})
+            if(producto){
+                res.status(200).json({
+                    message: 'Producto eliminado',
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'No se encontro el producto',
+                    success: false
+                })
+            }
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                message: 'Se produjo un error',
+                success: false
+            })
+        }
+    }
+}
+
+module.exports = productoController
