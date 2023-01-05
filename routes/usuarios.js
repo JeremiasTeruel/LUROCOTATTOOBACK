62e31e9edb5eeafacd8ba7cc18aae50c565dcf2b
Teje8 <jteruel8@gmail.com>
2023-01-05 20:34:07 -0300

    importacion de metodos del controlador y creacion de rutas

---
+++ routes/usuarios.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+let passport = require('../config/passport')
+let adminPassport = require('../config/adminPassport')
+const { registrarse, iniciarSesion, cerrarSesion, unUsuario, usuarios, editarUsuario, eliminarUsuario, verificarToken } = require('../controllers/usuarioController')
+
+
+router.get('/', usuarios);
+router.put('/', passport.authenticate('jwt', {session:false}), editarUsuario);
+router.post('/registrarse', registrarse);
+router.get('/token', passport.authenticate('jwt', {session:false}), verificarToken);
+router.post('/iniciarsesion', iniciarSesion);
+router.post('/cerrarsesion', cerrarSesion);
+router.get('/:id', unUsuario);
+router.delete('/:id', adminPassport.authenticate('jwt', {session:false}), eliminarUsuario)
+
+module.exports = router;
