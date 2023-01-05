1cd83178ef1d7d62264e360dc8a2975b83f71dc6
Teje8 <jteruel8@gmail.com>
2023-01-05 20:31:06 -0300

    configuracion para asegurar rutas

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+
+const {KEY_JWT} = process.env
+const Usuario = require('../models/Usuario')
+
+passport.use(
+    new passportJwt.Strategy(
+        {
+            jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: KEY_JWT
+        },
+        async (jwt_payload, done) => {
+            try{
+                let usuario = await Usuario.findOne({_id:jwt_payload.id})
+                if(usuario){
+                    usuario = {
+                        _id: usuario._id,
+                        nombre: usuario.nombre,
+                        mail: usuario.mail,
+                        foto: usuario.foto,
+                        role: usuario.role
+                    }
+                    return done(null, usuario)
+                } else {
+                    return done(null, false)
+                }
+            } catch(error){
+                console.log(error)
+                return done(error, false)
+            }
+        }
+    )
+)
+module.exports = passport
