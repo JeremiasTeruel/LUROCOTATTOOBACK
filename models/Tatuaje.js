63315653fe069729757be935f1e3f8581651d838
Teje8 <jteruel8@gmail.com>
2023-01-25 11:31:04 -0300

    cambio ref a artistas

--- models/Tatuaje.js
+++ models/Tatuaje.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const tatuajesSchema = new mongoose.Schema({
     imagen: {type: String, required: true,},
-    artista: {type: mongoose.Types.ObjectId, ref: 'artistas', required: true}
+    artista: {type: mongoose.Types.ObjectId, ref: 'artistas'}
 })
 
 const tatuajesModel = mongoose.model(
